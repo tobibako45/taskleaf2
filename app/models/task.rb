6f85aaeba189156d2f30811147bdeb5211d0824e
tobibako45 <tobibako45@gmail.com>
2019-10-17 01:29:51 +0900

    Add validate_name_not_comma

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,23 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
+  # 検証用メソッド「validate_name_not_including_comma」という名前にして、
+  # それをvalidateというクラスメソッドに渡すことにより、
+  # これを検証のために利用するということをTaskモデルクラスに知らせる
+  validate :validate_name_not_including_comma
+
+  private
+
+  # カンマではなく名前を検証する
+  def validate_name_not_comma
+    # カンマが含まれているかをinclude?でチェック。
+    # &.でnameがnilのときには検証が通る(errors.addしない)ようにしている
+    errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
+
+    # include? 含まれるときはtrue、含まれないときはfalse
+    # &. ぼっち演算子 xxxがnilでないときにメソッドyyyを呼び出す。
+  end
+
 end
+
