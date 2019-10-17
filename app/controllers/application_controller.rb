98d55f8aff767b32dd8eb6f6a9a255750c28afef
tobibako45 <tobibako45@gmail.com>
2019-10-17 16:03:12 +0900

    Add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # helper_method指定することで、すべてのビューからも使える
+  helper_method :current_user
+
+  private
+
+  # ログインしているユーザーを取得する処理は、頻繁に必要になるのでココに移動
+  def current_user
+    # a ||= xxx
+    # 「||」演算子の自己代入演算子。aが偽か未定義なら、aにxxxを代入する、という意味。a || (a = true) と同じ
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
