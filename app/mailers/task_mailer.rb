7487c3c39f704a916f8bc4bd65007f80eb833a80
tobibako45 <tobibako45@gmail.com>
2019-10-24 11:04:10 +0900

    メイラーの実装

---
+++ app/mailers/task_mailer.rb
@@ -0,0 +1,16 @@
+class TaskMailer < ApplicationMailer
+  # デフォルトの設定 fromのデフォルト
+  default form: 'taskleaf2@example.com'
+
+  # Task登録の通知メールを送る
+  def creation_email(task)
+    @task = task # テンプレートで利用したいので、インスタンス変数に
+    # mailメソッドでメールの作成・送信
+    mail(
+      subject: 'タスク制作完了メール',
+      to: 'user@example.com',
+      from: 'taskleaf2@example.com'
+    )
+  end
+
+end
